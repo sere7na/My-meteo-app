698adb3b2ae6ebf28671e6d25ad37d13dc900b84
sere7na <s.cand3lli@gmail.com>
2025-12-27 18:40:51 +0100

    Added search functionality using API

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
+function updatecityData(response) {
+let temperatureElement = document.querySelector("#temperature");
+let temperature = response.data.temperature.current;
+let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.city;
+temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+let apiKey = "9t597542a7ab8b73a9f4o3da60324dfc";
+let apiUrl = 
+`https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+axios.get(apiUrl).then(updatecityData);
+}
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = searchInput.value;
+    
+    searchCity(searchInput.value);
 
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Tokyo");
 
